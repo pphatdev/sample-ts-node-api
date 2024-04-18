8cd9650eedc39206e32fefd45a4ae019965f4e2c
HalloPhat <65520537+pphatdev@users.noreply.github.com>
2024-04-18 07:44:30 +0000

    Update Connection Info

--- configs/pg.ts
+++ configs/pg.ts
@@ -2,15 +2,22 @@ import pgsql from "pg"
 import { HOST, NAME, PORT, PWD, USER } from "./index.js";
 const { Client } = pgsql
 
-export const client = new Client({
+const config = {
     host: HOST,
     port: PORT,
     database: NAME,
     user: USER,
     password: PWD,
-});
+}
+
+export const client = new Client(config);
 
 client.connect((err) => {
-    if (err)
-        return console.error(`\ncould not connect to postgres '${err.message}\n`);
+    if (err) {
+        console.log(`\n\x1b[41mcould not connect to database:\x1b[0m`);
+        console.table([err])
+        console.log(`\n\n\x1b[41mConnection information:\x1b[0m`);
+        console.table([config])
+        return false
+    }
 });
